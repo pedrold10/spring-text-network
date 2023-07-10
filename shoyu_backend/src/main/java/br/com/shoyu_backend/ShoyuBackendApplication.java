f01dc8e46619ca367b3292bc934b80d7a1480529
pedrold10 <pedrogabriel12423@gmail.com>
2023-07-10 09:40:46 -0300

    Inicializando backend da aplicação

---
+++ shoyu_backend/src/main/java/br/com/shoyu_backend/ShoyuBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.shoyu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoyuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoyuBackendApplication.class, args);
+	}
+
+}

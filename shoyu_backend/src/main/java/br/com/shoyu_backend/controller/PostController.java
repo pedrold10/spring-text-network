2c8d6748b99b49709221229cd24c9b60e55e9ef2
pedrold10 <pedrogabriel12423@gmail.com>
2023-07-10 13:30:01 -0300

    Criando método de listagem de postagens

---
+++ shoyu_backend/src/main/java/br/com/shoyu_backend/controller/PostController.java
@@ -0,0 +1,25 @@
+package br.com.shoyu_backend.controller;
+
+import br.com.shoyu_backend.model.entities.Post;
+import br.com.shoyu_backend.model.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PostController{
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService){
+        this.postService = postService;
+    }
+
+    @RequestMapping(value = "/posts", method = RequestMethod.GET)
+    public List<Post> listPost(){
+        return postService.listPost();
+    }
+}
---
+++ shoyu_backend/src/main/java/br/com/shoyu_backend/model/services/PostService.java
@@ -0,0 +1,23 @@
+package br.com.shoyu_backend.model.services;
+
+import br.com.shoyu_backend.model.entities.Post;
+import br.com.shoyu_backend.model.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository){
+        this.postRepository = postRepository;
+    }
+
+    public List<Post> listPost(){
+        return postRepository.findAll();
+    }
+}
